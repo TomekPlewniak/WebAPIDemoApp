6cfa15cfaa1fad20688c004e3379dde881ce54e5
Tomasz Plewniak <tomekplewniak@gmail.com>
2019-06-26 20:07:49 +0200

    Add custom route. Update and enable automatic documentation.

--- DemoAPI/Controllers/PeopleController.cs
+++ DemoAPI/Controllers/PeopleController.cs
@@ -8,32 +8,63 @@ using System.Web.Http;
 
 namespace DemoAPI.Controllers
 {
+    /// <summary>
+    /// This is where I give You all the infromation about my peeps.
+    /// </summary>
     public class PeopleController : ApiController
     {
         List<Person> people = new List<Person>();
 
+        public PeopleController()
+        {
+            people.Add(new Person { FirstName = "Tomek", LastName = "Plewniak", Id = 1 });
+            people.Add(new Person { FirstName = "Sue", LastName = "Storm", Id = 2 });
+            people.Add(new Person { FirstName = "Bilbo", LastName = "Baggins", Id = 3 });
+        }
 
 
         // GET: api/People
         public List<Person> Get()
         {
-            return new string[] { "value1", "value2" };
+            return people;
+        }
+
+        /// <summary>
+        /// Gets a list of the first names of all user.
+        /// </summary>
+        /// <param name="userId">The uniqe identifier for this person.</param>
+        /// <param name="age">We whant to know how old they are.</param>
+        /// <returns>A lsit of first names...duh</returns>
+        [Route("api/People/GetFirstNames/{userId:int}/{age:int}")]
+        [HttpGet]
+        public List<string> GetFirstNames(int userId, int age)
+        {
+            List<string> output = new List<string>();
+
+            foreach (var p in people)
+            {
+                output.Add(p.FirstName);
+            }
+
+            return output;
         }
 
         // GET: api/People/5
-        public string Get(int id)
+        public Person Get(int id)
         {
-            return "value";
+            return people.Where(x => x.Id == id).FirstOrDefault();
         }
 
         // POST: api/People
-        public void Post([FromBody]string value)
+        public void Post(Person val)
         {
+            people.Add(val);
         }
 
         // DELETE: api/People/5
         public void Delete(int id)
         {
+
         }
     }
 }
--- DemoAPI/Models/Person.cs
+++ DemoAPI/Models/Person.cs
@@ -5,10 +5,24 @@ using System.Web;
 
 namespace DemoAPI.Models
 {
+    /// <summary>
+    /// Represent one specific person.
+    /// </summary>
     public class Person
     {
+        /// <summary>
+        /// ID from SQL
+        /// </summary>
         public int Id { get; set; } = 0;
+        
+        /// <summary>
+        /// The user's first name. 
+        /// </summary>
         public string FirstName { get; set; } = "";
+
+        /// <summary>
+        /// Couldn't you guess?
+        /// </summary>
         public string LastName { get; set; } = "";
     }
 }
